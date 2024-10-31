7fc25608ab5b89fe117d61bb5f299d7d83339da5
ThibaultDekock <thibault.dekock@student.kuleuven.be>
2024-10-31 09:30:20 +0100

    push

--- src/Edge.java
+++ src/Edge.java
@@ -1,15 +1,22 @@
 package src;
 
 public class Edge {
-    int id;
-    EdgeType edgeType;
-    int cost;
-    int endpoint1;
-    int endpoint2;
+    public int id;
+    public EdgeType edgeType;
+    public int cost;
+    public int endpoint1;
+    public int endpoint2;
 
     public enum EdgeType {
         REGULAR, OFFSTREET // Add other types if needed
     }
 
+    public Edge(int id, EdgeType edgeType, int cost, int endpoint1, int endpoint2) {
+        this.id = id;
+        this.edgeType = edgeType;
+        this.cost = cost;
+        this.endpoint1 = endpoint1;
+        this.endpoint2 = endpoint2;
+    }
     // Getters and setters can be added here if needed
 } 
